878d6d5bc67e8933a096827eeacd94b88b1da065
Barry Lin <tun32913@CIS-Linux2.temple.edu>
2023-02-01 19:52:58 -0500

    Test

--- Testing Folder/Untitled-1.c
+++ Testing Folder/Untitled-1.c
@@ -10,37 +10,61 @@ if the entry is a directory it will call function allFiles()recursively to read 
 #include <string.h>
 #include <sys/stat.h>
 
+void allFiles(const char *dirname, int indent);
+
+int main(int argc, const char *argv[]){
+            char path[6969];
+
+
+        allFiles(".", 0);
+   return 0;
+}
+
 void allFiles(const char *dirname, int indent){
-    //constructing a path
-    char path[6969];
+    int counter;
     int i;
+
+
     DIR *dir;
     dir = opendir(dirname); // want to see directories and files in current directory
     //if state ment if there is an error getting into a directory
-  if (!dir){
-    return;
-  }    
+    if (dir ==NULL){
+        perror("Error");
+        return;
+    }
+    
+    for(counter=0;counter<indent;counter++) printf(" ");
+        //printf("%s\n",dirname);
+        for(i = 0; i < 10; i++){
+             printf("%s\n",dirname);
+        }
+
+
+    
     struct dirent *entry;
 
     entry = readdir(dir); // This will give some entries or null 
 
     // while loop to keep reading (iterate) through everything
     while(entry != NULL){
+        //constructing a path
+        char path[6969];
         // if the entry is a directory it will open the read the subfolder
+    
         // "borrowed" and inpired by https://www.ibm.com/docs/en/zos/2.4.0?topic=functions-opendir-open-directory"
-          if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) { // compares entry name with "." and comapres with ".." will ignore current and parent folder
-            for (i=0; i<indent; i++) 
-            {
-                if (i%2 == 0 || i == 0)
-                    printf("%s", " ");
-                else
-                    printf(" ");
-            }
-            printf("%s%s\n", "  ", entry->d_name);
-            strcpy(path, dirname);
+        //prints file folder name.. etc in the current directory
+         printf("%s\n", entry->d_name);
+           if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0)
+        { // compares entry name with "." and comapres with ".." will ignore current and parent folder
+
+            
+
+
+            //concat dirname
+            strcat(path,dirname);
             strcat(path, "/");
-            strcat(path, entry->d_name);
-            allFiles(path, indent+4);
+            strcat(path,entry ->d_name);
+            allFiles(path,indent+4);
         
          }
          // shows next directory 
@@ -48,26 +72,5 @@ void allFiles(const char *dirname, int indent){
     }
 
 closedir(dir);
-}
-
-
-void allFiles(const char *dirname, int indent);
-
-int main(int argc, const char *argv[]){
-   if( argc == 2 ) {
-       DIR *directory = opendir(argv[1]);
-       if (directory){
-         allFiles(argv[1], 0);
-       }
-       else{
-         printf("cannot open directory");
-         return 1;
-       }
-     }
-     else {
-        
-        allFiles(".", 0);
-     }
-    return 0;
 }
 
