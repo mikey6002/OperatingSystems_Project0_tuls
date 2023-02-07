57ee931bd33812611cd9c05a470730fe2bdd8617
Barry <72505018+mikey6002@users.noreply.github.com>
2023-02-07 09:02:53 -0500

    Update tucp.c

--- Testing_Folder/Text_Folders/tucp.c
+++ Testing_Folder/Text_Folders/tucp.c
@@ -1,50 +1,128 @@
-#define _GNU_SOURCE
-
-#include <dirent.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
-#define Buffer_SIZE =6969
+#define BUFFER_SIZE 1024
 
+
+
+//https://stackoverflow.com/questions/7267295/how-can-i-copy-a-file-from-one-directory-to-another-in-c-c
+//Function to copy one file to another
+void copyFile(char* sourceFile, char* destFile) {
+   int srcFd, destFd;
+   char buffer[BUFFER_SIZE];
+   ssize_t bytes;
+
+   //Open source and destination files
+   srcFd = open(sourceFile, O_RDONLY);
+   destFd = open(destFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+   //Error checking
+   if (srcFd == -1) {
+       printf("Error: could not open source file\n");
+       exit(-1);
+   }
+   if (destFd == -1) {
+       printf("Error: could not open destination file\n");
+       exit(-1);
+   }
+
+   //Copy the source file to the destination file
+   while ((bytes = read(srcFd, buffer, BUFFER_SIZE)) > 0) {
+       write(destFd, buffer, bytes);
+   }
+
+   //Close the files
+   close(srcFd);
+   close(destFd);
+}
+
+//Function to copy multiple files to a directory
+void copyFilesToDir(char** sourceFiles, int numFiles, char* destDir) {
+   int srcFd, destFd;
+   char buffer[BUFFER_SIZE];
+   ssize_t bytes;
+
+   //Iterate through the source files
+   for (int i=0; i<numFiles; i++) {
+       //Create the full path for the destination file
+       char destFile[strlen(destDir) + strlen(sourceFiles[i]) + 1];
+       strcpy(destFile, destDir);
+       strcat(destFile, "/");
+       strcat(destFile, sourceFiles[i]);
+
+       //Open the source and destination files
+       srcFd = open(sourceFiles[i], O_RDONLY);
+       destFd = open(destFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+       //Error checking
+       if (srcFd == -1) {
+           printf("Error: could not open file %s\n", sourceFiles[i]);
+           exit(-1);
+       }
+       if (destFd == -1) {
+           printf("Error: could not open destination file %s\n", destFile);
+           exit(-1);
+       }
+
+       //Copy the source file to the destination file
+       while ((bytes = read(srcFd, buffer, BUFFER_SIZE)) > 0) {
+           write(destFd, buffer, bytes);
+       }
+
+       //Close the files
+       close(srcFd);
+       close(destFd);
+   }
+}
+
+//Function to check if the given path is a file or a directory
 int isDir(char* path) {
    struct stat st;
 
+   //Error checking
    if (stat(path, &st) == -1) {
        printf("Error: could not get information about %s\n", path);
-       return -1;
+       exit(-1);
    }
 
+   //Return 1 if the path is a directory, 0 otherwise
    return S_ISDIR(st.st_mode);
 }
 
+int main(int argc, char** argv) {
+   //Error checking
+   if (argc < 3) {
+       printf("Error: not enough arguments\n");
+       exit(-1);
+   }
 
-int main(int argc, char *argv[]) {
-    int DestinDir = isDir(argv[argc-1]);
+   //Check if the destination is a directory
+   int isDestDir = isDir(argv[argc-1]);
 
-    if(argc <3){
-        printf("Not Enough Arguements");
-        return;
-    }
+   //Copy one file to another
+   if ((argc == 3) && (!isDestDir)) {
+       copyFile(argv[1], argv[2]);
+   }
 
-    if((argc ==3)&& (!DestinDir)){
-        copyFile(argv[1],argv[2]);
+   //Copy one file to a directory
+   else if ((argc == 3) && (isDestDir)) {
+       copyFile(argv[1], argv[2]);
+   }
 
-    }
+   //Copy multiple files to a directory
+   else if ((argc > 3) && (isDestDir)) {
+       copyFilesToDir(argv, argc-2, argv[argc-1]);
+   }
 
-    else if((argc==3)&& (DestinDir)){
-       copyFile(argv[1],argv[2]);
-    }
-    
-    else if((argc >3)&& (DestinDir)){
-        copyFilesToDir(argv,argc-2,argv[argc-1]);
-    }
-    
-    else{
-        printf("invalid arguemnts");
-        exit(-1);
-    }
-   
-    return 0;
+   //Error
+   else {
+       printf("Error: invalid arguments\n");
+       exit(-1);
+   }
+
+   return 0;
 }
