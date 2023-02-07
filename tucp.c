83446ea8857a9103ce06f1747fe4980cbc452fdf
Barry <72505018+mikey6002@users.noreply.github.com>
2023-02-07 11:09:12 -0500

    Update tucp.c

--- tucp.c
+++ tucp.c
@@ -5,6 +5,10 @@
 #include <fcntl.h>
 #include <stdlib.h>
 
+#define BUFFER_SIZE 1024
+
+// Was in a time crunch studying for midterms  forgive me using a lot of stack overflows ;-;
+
 
 //https://stackoverflow.com/questions/7267295/how-can-i-copy-a-file-from-one-directory-to-another-in-c-c
 //Function to copy one file to another
@@ -37,6 +41,45 @@ void copyFile(char* sourceFile, char* destinFile) {
    close(destFd);
 }
 
+//https://stackoverflow.com/questions/15796503/copying-multiple-files-in-c
+//Function to copy multiple files to a directory
+void copyFilesToDir(char** sourceFiles, int numFiles, char* destDir) {
+   int sourceFd, destFd;
+   char buffer[BUFFER_SIZE];
+   ssize_t exp;
+
+   //Iterate through the source files
+   for (int i=0; i<numFiles; i++) {
+       //Create the full path for the destination file
+       char destinFile[strlen(destDir) + strlen(sourceFiles[i]) + 1];
+       strcpy(destinFile, destDir);
+       strcat(destinFile, "/");
+       strcat(destinFile, sourceFiles[i]);
+
+       //Open the source and destination files
+       sourceFd = open(sourceFiles[i], O_RDONLY);
+       destFd = open(destinFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+       //Error checking
+       if (sourceFd == -1) {
+           printf("Error: could not open file %s\n", sourceFiles[i]);
+            exit(-1);
+       }
+       if (destFd == -1) {
+           printf("Error: could not open destination file %s\n", destinFile);
+            exit(-1);
+       }
+
+       //Copy the source file to the destination file
+       while ((exp = read(sourceFd, buffer, BUFFER_SIZE)) > 0) {
+           write(destFd, buffer, exp);
+       }
+
+       //Close the files
+       close(sourceFd);
+       close(destFd);
+   }
+}
 
 
 //Function to check if the given path is a file or a directory
